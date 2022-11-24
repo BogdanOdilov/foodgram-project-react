5abe368ece27fc17f6092e376d40df78021e17c9
xudoyberdi <xudoyberdi.odilov@yandex.ru>
2022-11-24 20:14:15 +0300

    sprint 14 v17

--- backend/users/migrations/0001_initial.py
+++ backend/users/migrations/0001_initial.py
@@ -17,9 +17,19 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Follow',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='following', to=settings.AUTH_USER_MODEL, verbose_name='Автор')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='follower', to=settings.AUTH_USER_MODEL, verbose_name='Подписчик')),
+                ('id', models.AutoField(
+                    auto_created=True, primary_key=True,
+                    serialize=False, verbose_name='ID')),
+                ('author', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='following',
+                    to=settings.AUTH_USER_MODEL,
+                    verbose_name='Автор')),
+                ('user', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='follower',
+                    to=settings.AUTH_USER_MODEL,
+                    verbose_name='Подписчик')),
             ],
             options={
                 'verbose_name': 'Подписка',
@@ -29,6 +39,8 @@ class Migration(migrations.Migration):
         ),
         migrations.AddConstraint(
             model_name='follow',
-            constraint=models.UniqueConstraint(fields=('user', 'author'), name='unique follow'),
+            constraint=models.UniqueConstraint(
+                fields=('user', 'author'),
+                name='unique follow'),
         ),
     ]
