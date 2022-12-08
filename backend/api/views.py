1b368f5ed7a9964290e05d5afead9cc99bc62cf9
xudoyberdi <xudoyberdi.odilov@yandex.ru>
2022-12-09 01:22:16 +0300

    sprint 14 v29

--- backend/api/views.py
+++ backend/api/views.py
@@ -12,7 +12,6 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
